921f6d6484c9f42d94265e9dc50d6392d1133e9b
untserius <sudhirdas1999@gmail.com>
2023-08-01 17:43:50 +0530

    css modifications

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+// Hamburger Menu EventHandler
 const menuHamburger = document.querySelector(".menu-hamburger")
 const navLinks = document.querySelector(".nav-links")
-
-// const plusAdd = document.querySelector(".plus")
-// const shareButtons = document.querySelector(".share-buttons")
-
-
-// plusAdd.addEventListener("click", () => {
-
-// })
 
 menuHamburger.addEventListener("click", () => {
     navLinks.classList.toggle("mobile-menu")
     menuHamburger.classList.toggle("active")
-
 })
 
+// Plus Icon EventHandler
+const shareBtn = document.querySelector(".fa-plus")
+const shareMenu = document.querySelector(".share-buttons")
+
+// Initially hide the share menu on page load
+shareMenu.classList.remove("popup");
+
+shareBtn.addEventListener('click', () => {
+    shareMenu.classList.toggle("popup")
+})
